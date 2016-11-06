4d441748a4a50f647ea659236614ab977c145bec
Casper Oakley <casper_oakley@hotmail.com>
2016-11-06 17:42:41 +0000

    Moved frontend-based HSV calculations to backend

--- public/js/main.js
+++ public/js/main.js
@@ -3,25 +3,21 @@ var app = angular.module('myApp', ['ngAnimate']);
 
 app.controller('myCtrl', function($scope, $http, socket) {
   $scope.values = [];
-  $scope.max = 100000;
-  $scope.min = 0;
   for(var i=0;i<256;i++){
     $scope.values[i] = 0;
   }
 
   //Function to get the string for rgb for each pixel
   $scope.readingToHex = function(x) {
-    rgb = HSVtoRGB((2.0/3)* (1 - (Math.abs(x-$scope.min)/Math.abs(x-$scope.max))), 1, 1);
+    //rgb = HSVtoRGB((2.0/3)* (1 - (Math.abs(x-$scope.min)/Math.abs(x-$scope.max))), 1, 1);
     return '#' +
-           paddedToString(rgb.r) + 
-           paddedToString(rgb.g) + 
-           paddedToString(rgb.b);
+           paddedToString(x.r) + 
+           paddedToString(x.g) + 
+           paddedToString(x.b);
   };
 
   socket.on('reading', function (data) {
     $scope.values = data.readings;
-    $scope.max = data.max;
-    $scope.min = data.min;
   });
 
   $scope.valueToStyle = function(x) {
@@ -42,32 +38,6 @@ function paddedToString(x, n) {
     return x.toString(16);
   }
 };
-
-//Courtesy of Paul S., from stackoverflow
-function HSVtoRGB(h, s, v) {
-    var r, g, b, i, f, p, q, t;
-    if (arguments.length === 1) {
-        s = h.s, v = h.v, h = h.h;
-    }
-    i = Math.floor(h * 6);
-    f = h * 6 - i;
-    p = v * (1 - s);
-    q = v * (1 - f * s);
-    t = v * (1 - (1 - f) * s);
-    switch (i % 6) {
-        case 0: r = v, g = t, b = p; break;
-        case 1: r = q, g = v, b = p; break;
-        case 2: r = p, g = v, b = t; break;
-        case 3: r = p, g = q, b = v; break;
-        case 4: r = t, g = p, b = v; break;
-        case 5: r = v, g = p, b = q; break;
-    }
-    return {
-        r: Math.round(r * 255),
-        g: Math.round(g * 255),
-        b: Math.round(b * 255)
-    };
-}
 
 //Factory causing reapplies on any on event and emit event firing
 app.factory('socket', function ($rootScope) {
--- routes/socket.js
+++ routes/socket.js
@@ -16,11 +16,14 @@ module.exports = function (socket) {
             var readings = msg.content.toString().split(',').map(function(e) {
               return Math.abs(parseFloat(e));
             });
-            //Send readings to every socket
-            socket.sockets.emit('reading', {readings: readings,
-              max: Math.max.apply(Math, readings),
-              min: Math.min.apply(Math, readings)
+            var max = Math.max.apply(Math, readings),
+                min = Math.min.apply(Math, readings);
+            readings = readings.map(function(e) {
+              return readingToRGB(e, max, min);
             });
+            //console.log(readings);
+            //Send readings to every socket
+            socket.sockets.emit('reading', { readings: readings });
           }, {noAck: true});
 
           socket.on('connection', function() {
@@ -33,3 +36,35 @@ module.exports = function (socket) {
   });
   
 };
+
+var readingToRGB = function(x, max, min) {
+  return HSVtoRGB((2.0/3) * (1 - (Math.abs(x-min)/Math.abs(max-min))), 1, 1);
+};
+
+
+
+//Courtesy of Paul S., from stackoverflow
+var HSVtoRGB = function(h, s, v) {
+    var r, g, b, i, f, p, q, t;
+    if (arguments.length === 1) {
+        s = h.s, v = h.v, h = h.h;
+    }
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+    switch (i % 6) {
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+    return {
+        r: Math.round(r * 255),
+        g: Math.round(g * 255),
+        b: Math.round(b * 255)
+    };
+}
